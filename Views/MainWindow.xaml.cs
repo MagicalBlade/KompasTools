b59ffc08355a44dafd064d6f5ed300a5f304a013
Александр Воскобойник <magical.blade@gmail.com>
2023-03-14 21:11:12 +0300

    Добавьте файлы проекта.

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace KompasTools
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
