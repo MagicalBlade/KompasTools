da8356abec1af59bb91a09f7e31afeab76d4529d
Александр Воскобойник <magical.blade@gmail.com>
2025-10-15 08:40:09 +0300

    Запрет на запуск нескольких экземпляров приложения

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -14,5 +15,30 @@ namespace KompasTools
     /// </summary>
     public partial class App : Application
     {
+        #region Запрет на запуск нескольких экземпляров приложения
+        private static Mutex? mutex;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            const string appName = "KompasTools";
+
+            mutex = new Mutex(true, appName, out bool createdNew);
+
+            if (!createdNew)
+            {
+                // Приложение уже запущено
+                MessageBox.Show("Приложение уже запущено!");
+                Current.Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            mutex?.ReleaseMutex();
+            mutex?.Dispose();
+            base.OnExit(e);
+        } 
+        #endregion
     }
 }
