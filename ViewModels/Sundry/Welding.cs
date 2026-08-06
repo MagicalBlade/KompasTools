2bc58c897d01db63da86541742c5a1d9a7b3d65c
Александр Воскобойник <magical.blade@gmail.com>
2026-08-06 16:32:58 +0300

    LocationPart.Лево_Верх or LocationPart.Лево_Низ

--- ViewModels/Sundry/Welding.cs
+++ ViewModels/Sundry/Welding.cs
@@ -621,7 +621,7 @@ namespace KompasTools.ViewModels.Sundry
             #endregion
             Thickness1Str = "25"; //TODO Удалить
             Thickness2Str = "20"; //TODO Удалить
-            SelectTransitionTypes = TransitionTypeEnum.Вниз;
+            SelectTransitionTypes = TransitionTypeEnum.Вверх;
             Filter();
         }
         /// <summary>
