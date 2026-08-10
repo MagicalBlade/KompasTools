743addf6bb4f7bfec277946aebc8456836ccc2b3
Александр Воскобойник <magical.blade@gmail.com>
2026-08-10 09:58:01 +0300

    LocationPart.Лево_Верх or LocationPart.Лево_Низ

--- ViewModels/Sundry/Welding.cs
+++ ViewModels/Sundry/Welding.cs
@@ -619,9 +619,9 @@ namespace KompasTools.ViewModels.Sundry
                 WeldGOSTs = OrigWeldDates.Select(n => n.NameGost).Distinct().ToArray();
             }
             #endregion
-            Thickness1Str = "20"; //TODO Удалить
-            Thickness2Str = "25"; //TODO Удалить
-            SelectTransitionTypes = TransitionTypeEnum.Симметричный;
+            Thickness1Str = "25"; //TODO Удалить
+            Thickness2Str = "20"; //TODO Удалить
+            SelectTransitionTypes = TransitionTypeEnum.Вверх;
             SelectNameWeldJoints = "С15";
             Filter();
         }
